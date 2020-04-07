6a415dd0119c66d867b94724304f48a8ff04b4ca
dzikrisinatria <dzikri_alkautsar@yahoo.co.id>
2020-04-07 15:01:16 +0700

    Membuat design view Login dan menambahkan form_validation di library autoload

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,11 +6,10 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('form_validation');
     }
     
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('auth/login');
 	}
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,22 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
 
-    <!-- Bootstrap CSS-->
-    <link href="<?= base_url('assets/');?>css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap CSS-->
+	<link href="<?= base_url('assets/');?>css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Bootstrap core JavaScript-->
+	<!-- Bootstrap core JavaScript-->
 	<script src="<?= base_url('assets/');?>js/jquery.js"></script>
 	<script src="<?= base_url('assets/');?>js/bootstrap.bundle.min.js"></script>
 
 </head>
 
-<body>
-    
+<body class="h-100 bg-light">
+	<div class="container h-100">
+		<div class="row h-100 justify-content-center align-items-center">
+			<div class="col-10 col-md-8 col-lg-4">
+				<div class="card o-hidden border-0 shadow-lg">
+					<div class="card-body">
+						<div class="text-center">
+							<h1 class="h4 text-gray-900 mb-4">Login</h1>
+						</div>
+						
+						<?= $this->session->flashdata('message'); ?>
+
+						<form action="<?= base_url('auth');?>" method="post">
+							<div class="form-group">
+								<label for="username">Username</label>
+								<input type="text" class="form-control" id="username" name="username" value="<?= set_value('username'); ?>">
+								<?= form_error('username', '<small class="form-text text-danger">', '</small>'); ?>
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password" name="password">
+								<?= form_error('password', '<small class="form-text text-danger">', '</small>'); ?>
+							</div>
+							<div class="form-group">
+								<button type="submit" class="btn btn-success form-control mt-2">Login</button>
+							</div>
+							<hr>
+							<div class="text-center">
+								<a class="small" href="<?= base_url('auth/registration'); ?>">Belum memiliki akun? Silahkan <b>Register!</b></a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
