e004efabb15b22bd5193b6c98a8df9ce92aa6ff9
AndikaElang <49740810+AndikaElang@users.noreply.github.com>
2020-04-21 15:53:36 +0700

    Update M_obat.php

--- application/models/M_obat.php
+++ application/models/M_obat.php
@@ -13,8 +13,8 @@ class M_obat extends CI_Model
         if ($keyword){
             $this->cariuser($keyword);
         }
-		$this->db->join('user_role','user_role.role_id=user.role_id','LEFT OUTER');
-		$query = $this->db->get('user', $limit, $start);
+		$this->db->join('jenis_obat','jenis_obat.id_jenis_obat=obat.id_jenis_obat','LEFT OUTER');
+		$query = $this->db->get('obat', $limit, $start);
         return $query->result_array();
     }
     
@@ -23,14 +23,14 @@ class M_obat extends CI_Model
         if ($keyword){
             $this->carijenis($keyword);
         }
-		$query = $this->db->get('user_role', $limit, $start);
+		$query = $this->db->get('jenis_obat', $limit, $start);
         return $query->result_array();
     }
 
     public function totalRowsPagination($keyword)
     {
         $this->cariuser($keyword);
-        $this->db->join('user_role','user_role.role_id=user.role_id','LEFT OUTER');
+        $this->db->join('jenis_obat','jenis_obat.id_jenis_obat=obat.id_jenis_obat','LEFT OUTER');
         $this->db->from('user');
         return $this->db->count_all_results();
     }
@@ -38,29 +38,29 @@ class M_obat extends CI_Model
     public function totalRowsJenisPagination($keyword)
     {
         $this->carijenis($keyword);
-        $this->db->from('user_role');
+        $this->db->from('jenis_obat');
         return $this->db->count_all_results();
     }
 
-    public function getUserById($id_user)
+    public function getObatById($id_obat)
     {
-        $this->db->join('user_role','user_role.role_id=user.role_id','LEFT OUTER');
-        $this->db->where('id_user', $id_user);
-        return $this->db->get('user')->row_array();
+        $this->db->join('jenis_obat','jenis_obat.id_jenis_obat=obat.id_jenis_obat','LEFT OUTER');
+        $this->db->where('id_obat', $id_obat);
+        return $this->db->get('obat')->row_array();
     }
     
-    public function getUserCountByRole($role_id)
+    public function getObatCountByJenis($id_jenis_obat)
     {
-        $this->db->where('role_id', $role_id);
-        $this->db->from('user');
+        $this->db->where('id_jenis_obat', $id_jenis_obat);
+        $this->db->from('obat');
         return $this->db->count_all_results();
     }
 
     public function getAllObatAndJenis()
     {
 		$this->db->select('*');
-		$this->db->from('user');
-		$this->db->join('user_role','user_role.role_id=user.role_id','LEFT OUTER');
+		$this->db->from('obat');
+		$this->db->join('jenis_obat','jenis_obat.id_jenis_obat=obat.id_jenis_obat','LEFT OUTER');
 		$query = $this->db->get();
 		return $query->result_array();
     }
@@ -68,79 +68,79 @@ class M_obat extends CI_Model
     public function getAllJenis()
     {
 		$this->db->select('*');
-		$this->db->from('user_role');
+		$this->db->from('jenis_obat');
 		$query = $this->db->get();
 		return $query->result_array();
     }
 
-    public function countAllUser()
+    public function countAllObat()
     {
-        return $this->db->get('user')->num_rows();
+        return $this->db->get('obat')->num_rows();
     }
     
-    public function hapusUser($id_user)
+    public function hapusObat($id_obat)
     {
-        $this->db->where('id_user', $id_user);
-        return $this->db->delete('user');
+        $this->db->where('id_obat', $id_obat);
+        return $this->db->delete('obat');
     }
 
-    public function cariuser($keyword)
+    public function cariobat($keyword)
     {
-        $this->db->like('nama', $keyword);
-        $this->db->or_like('email', $keyword);
-        $this->db->or_like('username', $keyword);
-        $this->db->or_like('jenis_kelamin', $keyword);
-        $this->db->or_like('tgl_lahir', $keyword);
-        $this->db->or_like('alamat', $keyword);
-        $this->db->or_like('telepon', $keyword);
-        $this->db->or_like('role', $keyword);
+        $this->db->like('kode_obat', $keyword);
+        $this->db->or_like('nama_obat', $keyword);
+        $this->db->or_like('harga', $keyword);
+        $this->db->or_like('stok', $keyword);
+        $this->db->or_like('bentuk', $keyword);
+        $this->db->or_like('fungsi', $keyword);
+        $this->db->or_like('aturan', $keyword);
+        $this->db->or_like('nama_jenis', $keyword);
     }
     
     public function carijenis($keyword)
     {
-        $this->db->like('role_id', $keyword);
-        $this->db->or_like('role', $keyword);
+        $this->db->like('id_jenis_obat', $keyword);
+        $this->db->or_like('nama_jenis', $keyword);
     }
 
-    public function editdata($new_image)
+    public function editdataobat($new_image)
     {
         return $data = [
-            'nama'          => htmlspecialchars($this->input->post('nama', true)),
-            'email'         => htmlspecialchars($this->input->post('email', true)),
-            'username'      => htmlspecialchars($this->input->post('username', true)),
+            'kode_obat'          => $this->input->post('kode_obat'),
+            'nama_obat'         => $this->input->post('nama_obat'),
+            'harga'      => $this->input->post('harga'),
             // 'password'      => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-            'jenis_kelamin' => $this->input->post('jenis_kelamin'),
-            'tgl_lahir'     => $this->input->post('tgl_lahir'),
-            'alamat'        => $this->input->post('alamat'),
-            'telepon'       => $this->input->post('telepon'),
-            'foto'          => $new_image,
-            'role_id'       => $this->input->post('role'),
-            'date_created'  => time()
+            'stok' => $this->input->post('stok'),
+            'bentuk'     => $this->input->post('bentuk'),
+            'fungsi'        => $this->input->post('fungsi'),
+            'aturan'       => $this->input->post('aturan'),
+            'gambar'          => $new_image,
+            'id_jenis_obat'       => $this->input->post('jenis_obat'),
+            // 'date_created'  => time()
         ];
     }
 
-    public function adddata($new_image)
+    public function adddataobat($new_image)
     {
         return $data = [
-            'nama'          => htmlspecialchars($this->input->post('nama', true)),
-            'email'         => htmlspecialchars($this->input->post('email', true)),
-            'username'      => htmlspecialchars($this->input->post('username', true)),
-            'password'      => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-            'jenis_kelamin' => $this->input->post('jenis_kelamin'),
-            'tgl_lahir'     => $this->input->post('tgl_lahir'),
-            'alamat'        => $this->input->post('alamat'),
-            'telepon'       => $this->input->post('telepon'),
-            'foto'          => $new_image,
-            'role_id'       => $this->input->post('role'),
-            'date_created'  => time()
+            'kode_obat'          => $this->input->post('kode_obat'),
+            'nama_obat'         => $this->input->post('nama_obat'),
+            'harga'      => $this->input->post('harga'),
+            // 'password'      => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+            'stok' => $this->input->post('stok'),
+            'bentuk'     => $this->input->post('bentuk'),
+            'fungsi'        => $this->input->post('fungsi'),
+            'aturan'       => $this->input->post('aturan'),
+            'gambar'          => $new_image,
+            'id_jenis_obat'       => $this->input->post('jenis_obat'),
+            // 'date_created'  => time()
         ];
     }
 
-    public function updateUser($data,$id_user)
+    public function updateUser($data,$id_obat)
     {
         $this->db->set($data);
-        $this->db->where('id_user', $id_user);
-        $this->db->update('user');
+        $this->db->where('id_obat', $id_obat);
+        $this->db->update('obat');
     }
 
 }
